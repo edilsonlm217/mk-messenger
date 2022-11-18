71cf1a70190c148a922f55f45da3dfc1e8cf4553
Edilson Lima <edilson.lima.3@globalhitss.com.br>
2022-11-18 17:03:05 -0400

    Implementação da home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
 import styles from '../styles/styles.module.scss';
+import { WhatsappLogo } from 'phosphor-react';
 
 export default function Home() {
   return (
-    <h1>Home works!</h1>
+    <div className={styles.card}>
+      <div className={styles.cardHeader}>
+        <h1>Bem vindo ao MK-Messenger</h1>
+      </div>
+
+      <p>Você não possui uma sessão ativa no momento</p>
+
+      <div>
+        <WhatsappLogo size={96} color="#292929" weight="light" />
+      </div>
+
+      <p>Para iniciar sessão clique abaixo:</p>
+
+      <button className={styles.btnGenQrCode}>
+        Gerar QRCODE
+      </button>
+    </div>
   )
 }
