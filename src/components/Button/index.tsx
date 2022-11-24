5943855545d7889db9fe0b46cb90c37b0ec4d58b
Edilson Lima <edilson.lima.3@globalhitss.com.br>
2022-11-24 14:29:50 -0400

    Componentização dos Buttons do app

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import styles from "./styles.module.scss";
+
+interface ButtonProps {
+    children: string
+    primary?: boolean
+    secondary?: boolean
+}
+
+const Button: React.FC<ButtonProps> = (props) => {
+    if (props.primary || (!props.primary && !props.secondary)) {
+        return (
+            <button className={`${styles.btn} ${styles.btnPrimary}`}>
+                {props.children}
+            </button>
+        );
+    } else {
+        return (
+            <button className={`${styles.btn} ${styles.btnSecondary}`}>
+                {props.children}
+            </button>
+        );
+    }
+}
+
+export default Button;
