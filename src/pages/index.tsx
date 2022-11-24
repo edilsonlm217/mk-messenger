c7fa58a24fb5e428a2101a72f864414aa58cd239
Edilson Lima <edilson.lima.3@globalhitss.com.br>
2022-11-24 18:16:11 -0400

    Criação de cabeçalho e Texto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,23 @@
-import Button from '../components/Button';
 import BigWhiteCard from '../components/BigWhiteCard';
+import Heading from '../components/Heading';
+import Button from '../components/Button';
+import Text from '../components/Text';
 
 import styles from '../styles/styles.module.scss';
 
 export default function Home() {
   return (
     <div id="home" className={styles.pageContainer}>
-      <BigWhiteCard>
-        <h1>Hello World</h1>
+      <BigWhiteCard id="big-white-card">
+        <Heading fontSize="md" className={styles.cardHeading}>
+          Bem vindo ao <span>M</span><span>K</span>-<span>Messenger</span>
+        </Heading>
+
+        <Text body className={styles.text}>
+          Você não possui uma sessão ativa no momento
+        </Text>
       </BigWhiteCard>
-    </div>
+    </div >
   )
 }
 
