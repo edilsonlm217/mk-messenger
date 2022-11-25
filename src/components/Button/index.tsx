98c7a0a8b091b50f584d1194d222539323e2734b
Edilson Lima <edilson.lima.3@globalhitss.com.br>
2022-11-25 16:58:53 -0400

    Adição de onClick

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -11,13 +11,19 @@ interface ButtonProps {
 const Button: React.FC<ButtonProps & React.HTMLProps<HTMLButtonElement>> = (props) => {
     if (props.primary || (!props.primary && !props.secondary)) {
         return (
-            <button className={`${styles.btn} ${styles.btnPrimary}`}>
+            <button
+                className={`${styles.btn} ${styles.btnPrimary}`}
+                onClick={props.onClick}
+            >
                 {props.children}
             </button>
         );
     } else {
         return (
-            <button className={`${styles.btn} ${styles.btnSecondary}`}>
+            <button
+                className={`${styles.btn} ${styles.btnSecondary}`}
+                onClick={props.onClick}
+            >
                 {props.children}
             </button>
         );
