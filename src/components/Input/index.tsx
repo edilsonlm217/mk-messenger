313fa305c60d6b69b4389d83d8e78f8844d6b2e3
Edilson Lima <edilson.lima.3@globalhitss.com.br>
2022-11-25 17:00:13 -0400

    Componentização de input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import styles from './styles.module.scss';
+
+interface InputProps {
+    haserror: boolean
+    password?: boolean
+    placeholder?: string
+    label?: string
+}
+
+const Input: React.FC<InputProps & React.HTMLProps<HTMLInputElement>> = (props) => {
+    if (props.password) {
+        return (
+            <div className={`${styles.input} ${props.haserror && styles.inputAttentionField}`}>
+                <label htmlFor="password" className={styles.label}>Senha</label>
+                <input
+                    id="password"
+                    type="password"
+                    placeholder="Informe sua senha"
+                    required
+                    autoComplete="current-password"
+                />
+                {props.haserror &&
+                    <small>Por favor, preencha este campo corretamente.</small>
+                }
+            </div>
+        );
+    } else {
+        return (
+            <div className={`${styles.input} ${props.haserror && styles.inputAttentionField}`}>
+                <label htmlFor="text" className={styles.label}>
+                    {!props.label ? "Texto" : props.label}
+                </label>
+                <input
+                    id="text"
+                    type="text"
+                    placeholder={!props.placeholder ? "Texto" : props.placeholder}
+                    required={props.required}
+                    autoComplete="username"
+                />
+                {props.haserror &&
+                    <small>Por favor, preencha este campo corretamente.</small>
+                }
+            </div>
+        );
+    }
+}
+
+export default Input;
