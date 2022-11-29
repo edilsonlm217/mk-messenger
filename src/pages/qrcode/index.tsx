c6bb56cc1d860e1ff94d8e6111cb33fd25232d49
Edilson Lima <edilson.lima.3@globalhitss.com.br>
2022-11-28 22:26:38 -0400

    Implementação geração de QR Code

--- src/pages/qrcode/index.tsx
+++ src/pages/qrcode/index.tsx
@@ -1,13 +1,23 @@
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import React, { useEffect } from 'react';
+import { io } from "socket.io-client";
 import LocalStorage from '../../services/LocalStorage';
+
+import Text from '../../components/Text';
+import Button from '../../components/Button';
+import Heading from '../../components/Heading';
+import BigWhiteCard from '../../components/BigWhiteCard';
 
 import styles from './styles.module.scss';
 
 const Qrcode: React.FC = () => {
     const router = useRouter();
+    const [qrCode, setQRcode] = useState('');
 
-    useEffect(() => { redirectIfSessionNameDoesNotExist() }, []);
+    useEffect(() => {
+        redirectIfSessionNameDoesNotExist();
+        generateQrCode();
+    }, []);
 
     function redirectIfSessionNameDoesNotExist(): void {
         if (!LocalStorage.sessionNameExists()) {
@@ -15,8 +25,59 @@ const Qrcode: React.FC = () => {
         }
     }
 
+    async function generateQrCode() {
+        const sessionName = LocalStorage.getItem("client-session-name");
+
+        const socket = io(`http://localhost:3333`, {
+            query: {
+                client: sessionName
+            }
+        });
+
+        socket.on("successfully-connected", async () => {
+            console.log("Conectado com sucesso");
+
+            socket.on("qr-code-generation-started", () => {
+                console.log("Loading iniciado");
+            });
+
+            socket.on("qr-code-generation-done", ({ qrcode }) => {
+                console.log("Loading Finalizado");
+                setQRcode(qrcode);
+            });
+
+            socket.on("qr-code-generation-expired", () => {
+                console.log("Exibir botão [Gerar QrCode] Novamente");
+            });
+
+            socket.on("disconnect", () => {
+                socket.disconnect();
+                console.log('Servidor Desconectou');
+            });
+        });
+    }
+
     return (
-        <h1>QRCode works!</h1>
+        <div id="qrcode" className={styles.pageContainer}>
+            <BigWhiteCard id="big-white-card" className={styles.bigWhiteCard}>
+                <Heading fontSize="xs" className={styles.title}>
+                    Iniciar Sessão
+                </Heading>
+
+                <Text body className={styles.text}>
+                    Vá em aparelhos conectados no seu whatsapp e aponte para o
+                    QRCODE abaixos
+                </Text>
+
+                <div className={styles.qrCodeContainer}>
+                    <img src={qrCode} alt="qrCode" />
+                </div>
+
+                <div className={styles.cancelOperationBtn}>
+                    <Button>Cancelar Operação</Button>
+                </div>
+            </BigWhiteCard>
+        </div>
     );
 }
 
