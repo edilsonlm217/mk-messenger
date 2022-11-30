664917a288fcffe542a2a81fe9be403488b62e0f
Edilson Lima <edilson.lima.3@globalhitss.com.br>
2022-11-30 19:47:44 -0400

    Implementação de loader globalScope

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
-import '../styles/globals.scss';
+import React, { useEffect, useRef } from 'react';
 import type { AppProps } from 'next/app';
+import LoaderService from '../services/Loader';
+import { LoaderWrapper } from '../components/Loader/Wrapper';
+
+import '../styles/globals.scss';
+import 'semantic-ui-css/semantic.min.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const globalLoaderlRef = useRef(null);
+
+  useEffect(() => {
+    LoaderService.registerModal(globalLoaderlRef.current);
+  }, []);
+
+  return (
+    <>
+      <Component {...pageProps} />
+      <LoaderWrapper ref={globalLoaderlRef} />
+    </>
+  );
 }
