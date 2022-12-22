5fadd6224349c02de65a53825c41aba99043066e
Edilson Lima <edilson.lima.3@globalhitss.com.br>
2022-12-22 12:22:13 -0400

    Implementação de loading no componente butão

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactLoading from "react-loading";
 
 import styles from "./styles.module.scss";
 
@@ -6,25 +7,37 @@ interface ButtonProps {
     children: string
     primary?: boolean
     secondary?: boolean
+    isLoading?: boolean
 }
 
 const Button: React.FC<ButtonProps & React.HTMLProps<HTMLButtonElement>> = (props) => {
+    function handleOnClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void {
+        if (!props.isLoading && props.onClick) {
+            props.onClick(e);
+        }
+    }
     if (props.primary || (!props.primary && !props.secondary)) {
         return (
             <button
                 className={`${styles.btn} ${styles.btnPrimary}`}
-                onClick={props.onClick}
+                onClick={handleOnClick}
             >
-                {props.children}
+                {props.isLoading
+                    ? <ReactLoading type="spin" height={15} width={15} color="#fff" />
+                    : props.children
+                }
             </button>
         );
     } else {
         return (
             <button
                 className={`${styles.btn} ${styles.btnSecondary}`}
-                onClick={props.onClick}
+                onClick={handleOnClick}
             >
-                {props.children}
+                {props.isLoading
+                    ? <ReactLoading type="spin" height={10} width={10} color="#fff" />
+                    : props.children
+                }
             </button>
         );
     }
